ce8144bdf13e9e164f9f9daee656ef439a85d15a
Hasan Özdemir <34897251+hasanozdem1r@users.noreply.github.com>
2020-02-06 18:52:33 +0300

    Add files via upload

---
+++ cosinusSimilarity.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+text=["London Paris London","Paris Paris London"]
+cv=CountVectorizer()
+count_matrix=cv.fit_transform(text)
+print(count_matrix.toarray())
+
+sim_scores=cosine_similarity(count_matrix)
+print(sim_scores)
